5aa94f7701fef8c32cbfbf0104823768647702c0
joanpurbaa <joanpurba562@gmail.com>
2024-12-10 20:20:18 +0700

    feat : do chevalier task

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("hello world");
-  }
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> nilaiMahasiswa = new ArrayList<>();
+        
+        nilaiMahasiswa.add(1);
+        nilaiMahasiswa.add(3);
+        nilaiMahasiswa.add(5);
+        nilaiMahasiswa.add(-8);
+        nilaiMahasiswa.add(12);
+        nilaiMahasiswa.add(90);
+        nilaiMahasiswa.add(3);
+        nilaiMahasiswa.add(7);
+
+        int target = 5;
+        boolean ditemukan = false;
+
+        for (int i = 0; i < nilaiMahasiswa.size(); i++) {
+            if (nilaiMahasiswa.get(i) == target) {
+                System.out.println("Ditemukan di indeks ke-" + i);
+                ditemukan = true;
+                break;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Tidak ditemukan");
+        }
+    }
 }
