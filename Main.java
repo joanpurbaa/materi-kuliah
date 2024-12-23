6a2b44acb9a4f2ae4ce2a6fe28b2ac988c3c3a07
joanpurbaa <joanpurba562@gmail.com>
2024-12-23 11:53:05 +0700

    feat : do 3rd meet task

--- Main.java
+++ Main.java
@@ -1,31 +1,39 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> nilaiMahasiswa = new ArrayList<>();
-        
-        nilaiMahasiswa.add(1);
-        nilaiMahasiswa.add(3);
-        nilaiMahasiswa.add(5);
-        nilaiMahasiswa.add(-8);
-        nilaiMahasiswa.add(12);
-        nilaiMahasiswa.add(90);
-        nilaiMahasiswa.add(3);
-        nilaiMahasiswa.add(7);
+        try(Scanner userInput = new Scanner(System.in)){
+            int ukuran = userInput.nextInt();
+            String benda[] = new String[ukuran];
+            int counter = 0;
 
-        int target = 5;
-        boolean ditemukan = false;
+            while(ukuran != counter){
+                benda[counter] = userInput.next();
+                counter++;
+            }
 
-        for (int i = 0; i < nilaiMahasiswa.size(); i++) {
-            if (nilaiMahasiswa.get(i) == target) {
-                System.out.println("Ditemukan di indeks ke-" + i);
-                ditemukan = true;
-                break;
+            for(int i = 0; i < benda.length; i++){
+                for(int j = 0; j < benda.length - i - 1; j++){
+                    if(compareTo(benda[j], benda[j + 1]) > 0){
+                        String temp = benda[j];
+                        benda[j] = benda[j + 1];
+                        benda[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (String i : benda) {
+                System.out.print(i + " ");
             }
         }
+    }
 
-        if (!ditemukan) {
-            System.out.println("Tidak ditemukan");
+    public static int compareTo(String s1, String s2) {
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            int diff = s1.charAt(i) - s2.charAt(i);
+            if (diff != 0) return diff;
         }
+        
+        return s1.length() - s2.length();
     }
 }
